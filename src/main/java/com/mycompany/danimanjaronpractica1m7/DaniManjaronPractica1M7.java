693c1c51bc34b4f336068213d2b85a1047f34d0f
dmanjaron <danielmanjaron@aluminsestatut.cat>
2024-09-22 18:40:15 +0200

    first commit of the Calculator project

---
+++ src/main/java/com/mycompany/danimanjaronpractica1m7/DaniManjaronPractica1M7.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.danimanjaronpractica1m7;
+
+import java.awt.Color;
+import java.awt.Frame;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author danim
+ */
+public class DaniManjaronPractica1M7 {
+
+    public static void main(String[] args) {
+       Calculadora calculadora = new Calculadora();
+       calculadora.setVisible(true);
+        
+    }
+}
